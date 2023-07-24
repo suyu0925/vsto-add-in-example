60bc21e96786e2d7b5cd7c0feca198a72f68473c
Su Yu <suyu0925@gmail.com>
2023-07-24 09:52:31 +0800

    init dev branch

--- ExcelAddIn/Ribbon.cs
+++ ExcelAddIn/Ribbon.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using Office = Microsoft.Office.Core;
 
@@ -48,9 +52,157 @@ namespace ExcelAddIn
         public bool IsControlVisible(Office.IRibbonControl control)
         {
             return true;
-        }        
+        }
+
+        public void OnDelayDo(Office.IRibbonControl control)
+        {
+            Globals.ThisAddIn.InvokeAsyncCallToExcel();
+            // Thread t = new Thread(() =>
+            // {
+            //     MessageFilter.Register();
+            //     Thread.Sleep(2000);
+            //     try
+            //     {
+            //         Globals.ThisAddIn.Application.ActiveCell.Value2 =
+            //         DateTime.Now.ToShortTimeString();
+            //     }
+            //     catch (Exception ex)
+            //     {
+            //         Debug.WriteLine(ex.ToString());
+            //     }
+            // });
+            // t.SetApartmentState(ApartmentState.STA);
+            // t.Start();
+        }
+
+        public void OnDoSomethingOnThread(Office.IRibbonControl control)
+        {
+            Thread t = new Thread(() =>
+            {
+                MessageFilter.Register();
+                CallExcel();
+            });
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+        }
+
+        public async void OnDoSomething(Office.IRibbonControl control)
+        {
+            MessageFilter.Register();
+            await Task.Run(new System.Action(CallExcel));
+        }
+
+        public void OnHelp(Office.IRibbonControl control)
+        {
+            MessageBox.Show("help");
+        }
         #endregion
 
+        private void CallExcel()
+        {
+            try
+            {
+                var currSheet = (Worksheet)Globals.ThisAddIn.Application.ActiveSheet;
+                int rowSize = 50;
+                int colSize = 50;
+
+                for (int i = 1; i <= rowSize; i++)
+                    for (int j = 1; j <= colSize; j++)
+                        ((Range)currSheet.Cells[i, j]).Value2 = "sample";
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+        }
+
+        public class MessageFilter : IOleMessageFilter
+        {
+            //
+            // Class containing the IOleMessageFilter
+            // thread error-handling functions.
+
+            // Start the filter.
+            public static void Register()
+            {
+                IOleMessageFilter newFilter = new MessageFilter();
+                IOleMessageFilter oldFilter = null;
+                CoRegisterMessageFilter(newFilter, out oldFilter);
+                Debug.WriteLine($"MessageFilter.Register, oldFilter: {oldFilter}");
+            }
+
+            // Done with the filter, close it.
+            public static void Revoke()
+            {
+                IOleMessageFilter oldFilter = null;
+                CoRegisterMessageFilter(null, out oldFilter);
+                Debug.WriteLine($"MessageFilter.Revoke, oldFilter: {oldFilter}");
+            }
+
+            // Implement the IOleMessageFilter interface.
+            [DllImport("Ole32.dll")]
+            private static extern int
+              CoRegisterMessageFilter(IOleMessageFilter newFilter, out
+              IOleMessageFilter oldFilter);
+
+            //
+            // IOleMessageFilter functions.
+            // Handle incoming thread requests.
+            int IOleMessageFilter.HandleInComingCall(int dwCallType,
+              System.IntPtr hTaskCaller, int dwTickCount, System.IntPtr
+              lpInterfaceInfo)
+            {
+                //Return the flag SERVERCALL_ISHANDLED.
+                return 0;
+            }
+
+            // Thread call was rejected, so try again.
+            int IOleMessageFilter.RetryRejectedCall(System.IntPtr
+              hTaskCallee, int dwTickCount, int dwRejectType)
+            {
+                Debug.WriteLine($"RetryRejectedCall {hTaskCallee} {dwTickCount} {dwRejectType}");
+                if (dwRejectType == 2)
+                // flag = SERVERCALL_RETRYLATER.
+                {
+                    // Retry the thread call immediately if return >=0 & <100.
+                    // COM will wait for this many milliseconds and then retry the call.
+                    return 200;
+                }
+                // Too busy; cancel call.
+                return -1;
+            }
+
+            int IOleMessageFilter.MessagePending(System.IntPtr hTaskCallee,
+              int dwTickCount, int dwPendingType)
+            {
+                //Return the flag PENDINGMSG_WAITDEFPROCESS.
+                return 2;
+            }
+        }
+
+        [ComImport(), Guid("00000016-0000-0000-C000-000000000046"),
+        InterfaceTypeAttribute(ComInterfaceType.InterfaceIsIUnknown)]
+        interface IOleMessageFilter
+        {
+            [PreserveSig]
+            int HandleInComingCall(
+                int dwCallType,
+                IntPtr hTaskCaller,
+                int dwTickCount,
+                IntPtr lpInterfaceInfo);
+
+            [PreserveSig]
+            int RetryRejectedCall(
+                IntPtr hTaskCallee,
+                int dwTickCount,
+                int dwRejectType);
+
+            [PreserveSig]
+            int MessagePending(
+                IntPtr hTaskCallee,
+                int dwTickCount,
+                int dwPendingType);
+        }
         #region Helpers
 
         private static string GetResourceText(string resourceName)
@@ -72,7 +224,6 @@ namespace ExcelAddIn
             }
             return null;
         }
-
         #endregion
     }
 }
--- ExcelAddIn/ThisAddIn.cs
+++ ExcelAddIn/ThisAddIn.cs
@@ -6,10 +6,39 @@ using System.Xml.Linq;
 using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 using Microsoft.Office.Tools.Excel;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace ExcelAddIn
 {
-    public partial class ThisAddIn
+    [ComImport(), Guid("00000016-0000-0000-C000-000000000046"),
+    InterfaceTypeAttribute(ComInterfaceType.InterfaceIsIUnknown)]
+    interface IOleMessageFilter
+    {
+        [PreserveSig]
+        int HandleInComingCall(
+            int dwCallType,
+            IntPtr hTaskCaller,
+            int dwTickCount,
+            IntPtr lpInterfaceInfo);
+
+        [PreserveSig]
+        int RetryRejectedCall(
+            IntPtr hTaskCallee,
+            int dwTickCount,
+            int dwRejectType);
+
+        [PreserveSig]
+        int MessagePending(
+            IntPtr hTaskCallee,
+            int dwTickCount,
+            int dwPendingType);
+    }
+
+    public partial class ThisAddIn : IOleMessageFilter
     {
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
@@ -17,6 +46,90 @@ namespace ExcelAddIn
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
+        }
+
+        //
+        // IOleMessageFilter functions.
+        // Handle incoming thread requests.
+        int IOleMessageFilter.HandleInComingCall(int dwCallType,
+          System.IntPtr hTaskCaller, int dwTickCount, System.IntPtr
+          lpInterfaceInfo)
+        {
+            //Return the flag SERVERCALL_ISHANDLED.
+            return 0;
+        }
+
+        // Thread call was rejected, so try again.
+        int IOleMessageFilter.RetryRejectedCall(System.IntPtr
+          hTaskCallee, int dwTickCount, int dwRejectType)
+        {
+            if (dwRejectType == 2)
+            // flag = SERVERCALL_RETRYLATER.
+            {
+                // Retry the thread call immediately if return >=0 & 
+                // <100.
+                return 99;
+            }
+            // Too busy; cancel call.
+            return -1;
+        }
+
+        int IOleMessageFilter.MessagePending(System.IntPtr hTaskCallee,
+          int dwTickCount, int dwPendingType)
+        {
+            //Return the flag PENDINGMSG_WAITDEFPROCESS.
+            return 2;
+        }
+
+        // Implement the IOleMessageFilter interface.
+        [DllImport("Ole32.dll")]
+        private static extern int
+          CoRegisterMessageFilter(IOleMessageFilter newFilter, out
+          IOleMessageFilter oldFilter);
+
+        private IOleMessageFilter oldMessageFilter;
+
+        internal void InvokeAsyncCallToExcel()
+        {
+            Thread t = new Thread(this.RegisterFilter);
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+        }
+
+        private void RegisterFilter()
+        {
+            int a = CoRegisterMessageFilter(this, out oldMessageFilter);
+            Debug.WriteLine($"CoRegisterMessageFilter return {a}");
+            Thread.Sleep(3000);
+            CallExcel();
+        }
+
+        private void heavy()
+        {
+            var currSheet = (Excel.Worksheet)Globals.ThisAddIn.Application.ActiveSheet;
+            int rowSize = 50;
+            int colSize = 50;
+
+            for (int i = 1; i <= rowSize; i++)
+                for (int j = 1; j <= colSize; j++)
+                    ((Excel.Range)currSheet.Cells[i, j]).Value2 = "sample";
+        }
+
+        private void light()
+        {
+            this.Application.ActiveCell.Value2 = DateTime.Now.ToShortTimeString();
+        }
+
+        private void CallExcel()
+        {
+            try
+            {
+                heavy();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
         }
 
         protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
